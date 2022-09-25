35d4550c3b6cb2c986d93c4c77d0fe2c411c7ca2
Cristian Mejia <crmejiam120@gmail.com>
2022-09-25 18:19:50 -0500

    Añadir procedimiento 1 PL/SQL

--- trabajo.sql
+++ trabajo.sql
@@ -37,3 +37,31 @@ BEGIN
     :NEW.s_acumulado := 0;
   END IF;  
 END;
+
+/* Procedimiento 1 PL/SQL */
+
+CREATE OR REPLACE PROCEDURE consulta_cooperativa 
+(cod_coope IN cooperativa.codigo%TYPE) IS
+CURSOR socios_coope IS SELECT socio, sc_acumulado FROM coopexsocio WHERE coope = cod_coope;
+datos_cooperativa cooperativa%ROWTYPE;
+nom_socio socio.nombre%TYPE;
+count_socios NUMBER(5) := 1;
+num_socios NUMBER(8);
+total_valores NUMBER(8,3);
+BEGIN
+    SELECT * INTO datos_cooperativa FROM cooperativa WHERE codigo = cod_coope;
+    SELECT COUNT(*) INTO num_socios FROM coopexsocio WHERE coope = cod_coope GROUP BY coope;
+    SELECT SUM(sc_acumulado) INTO total_valores FROM coopexsocio WHERE coope = cod_coope GROUP BY coope;
+    DBMS_OUTPUT.PUT_LINE('Nombre de la cooperativa: '|| datos_cooperativa.nombre);
+    DBMS_OUTPUT.PUT_LINE('Acumulado de la cooperativa: '|| datos_cooperativa.c_acumulado);
+    DBMS_OUTPUT.PUT_LINE('Número de socios: '|| num_socios);
+    DBMS_OUTPUT.PUT_LINE('Socios de la cooperativa:');
+    DBMS_OUTPUT.PUT_LINE('{');
+    FOR iter_socio IN socios_coope LOOP
+        SELECT nombre INTO nom_socio FROM socio WHERE idsocio = iter_socio.socio;
+        DBMS_OUTPUT.PUT_LINE(count_socios || '. (Nombre: '|| nom_socio || ', Valorsc: ' || iter_socio.sc_acumulado || ')');
+        num_socios := num_socios + 1;
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE('}');
+    DBMS_OUTPUT.PUT_LINE('Total valores de los socios en la cooperativa: '|| total_valores);
+END;
